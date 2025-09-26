3af593e473b0366791b2030166d568258dd9219d
Dinmir331 <temirbaidildin@gmail.com>
2025-09-26 21:28:43 +0700

    stm32f411ceu6 led worked

--- build_F411CE/main.d
+++ build_F411CE/main.d
@@ -1,1 +1,14 @@
-build_F411CE/main.o: Core/Src/main.c
+build_F411CE/main.o: Core/Src/main.c \
+ CMSIS/Devices/STM32F4xx/Inc/STM32F411xE/stm32f411xe.h \
+ CMSIS/Include/core_cm4.h CMSIS/Include/cmsis_version.h \
+ CMSIS/Include/cmsis_compiler.h CMSIS/Include/cmsis_gcc.h \
+ CMSIS/Include/mpu_armv7.h CMSIS/Devices/STM32F4xx/Inc/system_stm32f4xx.h \
+ CMSIS/Devices/STM32F4xx/Inc/stm32f4xx.h
+CMSIS/Devices/STM32F4xx/Inc/STM32F411xE/stm32f411xe.h:
+CMSIS/Include/core_cm4.h:
+CMSIS/Include/cmsis_version.h:
+CMSIS/Include/cmsis_compiler.h:
+CMSIS/Include/cmsis_gcc.h:
+CMSIS/Include/mpu_armv7.h:
+CMSIS/Devices/STM32F4xx/Inc/system_stm32f4xx.h:
+CMSIS/Devices/STM32F4xx/Inc/stm32f4xx.h:
