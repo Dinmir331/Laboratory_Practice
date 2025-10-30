0a6ee3eb1e2c18e6949972f5eb92c7d331bcaa06
Dinmir331 <temirbaidildin@gmail.com>
2025-10-30 17:40:59 +0700

    diversification completed

--- build_F411CE/main.d
+++ build_F411CE/main.d
@@ -1,12 +1,3 @@
-build_F411CE/main.o: Core/Src/main.c \
- CMSIS/Devices/STM32F4xx/Inc/STM32F411xE/stm32f411xe.h \
- CMSIS/Include/core_cm4.h CMSIS/Include/cmsis_version.h \
- CMSIS/Include/cmsis_compiler.h CMSIS/Include/cmsis_gcc.h \
- CMSIS/Include/mpu_armv7.h CMSIS/Devices/STM32F4xx/Inc/system_stm32f4xx.h
-CMSIS/Devices/STM32F4xx/Inc/STM32F411xE/stm32f411xe.h:
-CMSIS/Include/core_cm4.h:
-CMSIS/Include/cmsis_version.h:
-CMSIS/Include/cmsis_compiler.h:
-CMSIS/Include/cmsis_gcc.h:
-CMSIS/Include/mpu_armv7.h:
-CMSIS/Devices/STM32F4xx/Inc/system_stm32f4xx.h:
+build_F411CE/main.o: Core/Src/main.c Core/Inc/init.h Core/Inc/main.h
+Core/Inc/init.h:
+Core/Inc/main.h:
