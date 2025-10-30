b8443fba65893189e88cd09e4afc85aedd486696
Dinmir331 <temirbaidildin@gmail.com>
2025-10-30 16:59:57 +0700

    all realizations v2

--- build_F411CE/main.d
+++ build_F411CE/main.d
@@ -2,8 +2,7 @@ build_F411CE/main.o: Core/Src/main.c \
  CMSIS/Devices/STM32F4xx/Inc/STM32F411xE/stm32f411xe.h \
  CMSIS/Include/core_cm4.h CMSIS/Include/cmsis_version.h \
  CMSIS/Include/cmsis_compiler.h CMSIS/Include/cmsis_gcc.h \
- CMSIS/Include/mpu_armv7.h CMSIS/Devices/STM32F4xx/Inc/system_stm32f4xx.h \
- CMSIS/Devices/STM32F4xx/Inc/stm32f4xx.h
+ CMSIS/Include/mpu_armv7.h CMSIS/Devices/STM32F4xx/Inc/system_stm32f4xx.h
 CMSIS/Devices/STM32F4xx/Inc/STM32F411xE/stm32f411xe.h:
 CMSIS/Include/core_cm4.h:
 CMSIS/Include/cmsis_version.h:
@@ -11,4 +10,3 @@ CMSIS/Include/cmsis_compiler.h:
 CMSIS/Include/cmsis_gcc.h:
 CMSIS/Include/mpu_armv7.h:
 CMSIS/Devices/STM32F4xx/Inc/system_stm32f4xx.h:
-CMSIS/Devices/STM32F4xx/Inc/stm32f4xx.h:
